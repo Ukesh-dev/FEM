f261ef77b07b0bfbfad19fbe49a9a593ec9f84ed
Ukesh <uks56shr567@gmail.com>
2021-10-20 08:09:11 +0545

    First Commit

---
+++ dist/script.js
@@ -0,0 +1,2 @@
+"use strict";fetch("https://google-search3.p.rapidapi.com/api/v1/search/q=elon+musk&num=100",{method:"GET",headers:{"x-user-agent":"desktop","x-rapidapi-host":"google-search3.p.rapidapi.com","x-rapidapi-key":"2894d80d96mshb91629339da585ep1da282jsn9fba9d22ef64"}}).then((function(e){console.log(e)})).catch((function(e){console.error(e)}));
+//# sourceMappingURL=script.js.map
